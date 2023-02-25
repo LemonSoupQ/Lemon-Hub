8b1411ab016c54cb5f9e4b6dc8dbabb1554b5787
LemonSoupQ <126293403+LemonSoupQ@users.noreply.github.com>
2023-02-25 13:09:58 +0200

    Update Lemon Hub.lua

--- Lemon Hub.lua
+++ Lemon Hub.lua
@@ -1,8 +1,8 @@
 if game.PlaceId == 286090429 then -- Arsenal
-     loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsena1.lua'),true))()   
+     loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsenal.lua'),true))()   
 end
 if game.PlaceId == 301549746 then -- Counter Blox
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Bl0x.lua'),true))()
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Blox.lua'),true))()
 end
 if game.PlaceId == 1345139196 then -- Treasure Hunt Simulator
  loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Treasure%20Hunt%20Simulator.lua'),true))()
