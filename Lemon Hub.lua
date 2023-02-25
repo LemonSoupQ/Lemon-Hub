ff345a86ee44591a285a96d733d42450e74106ea
LemonSoupQ <126293403+LemonSoupQ@users.noreply.github.com>
2023-02-25 02:11:43 +0200

    Update Lemon Hub.lua

--- Lemon Hub.lua
+++ Lemon Hub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 286090429 then -- Arsenal
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/LemonSoupQ/Universal-Aimbot/Lemon-hub/Arsenal.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsena1.lua"))()
 end
 if game.PlaceId == 301549746 then -- Counter Blox
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/LemonSoupQ/Universal-Aimbot/Lemon-hub/Counter%20Blox.lua"))()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Bl0x.lua"))()
