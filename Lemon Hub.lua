2e36975178429914914f601214e76fa586123979
LemonSoupQ <126293403+LemonSoupQ@users.noreply.github.com>
2023-02-26 13:32:08 +0200

    Update Lemon Hub.lua

--- Lemon Hub.lua
+++ Lemon Hub.lua
@@ -1,50 +1,50 @@
 if game.PlaceId == 286090429 then -- Arsenal
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsenal.lua'),true))()   
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsenal.lua'),true))()   
 end
 if game.PlaceId == 301549746 then -- Counter Blox
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Blox.lua'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Blox.lua'),true))()
 end
 if game.PlaceId == 1345139196 then -- Treasure Hunt Simulator
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Treasure%20Hunt%20Simulator.lua'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Treasure%20Hunt%20Simulator.lua'),true))()
 end
 if game.PlaceId == 292439477 then -- Phantom Forces
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Phantom%20Forces.lua'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Phantom%20Forces.lua'),true))()
 end
 if game.PlaceId == 2753915549 then -- Blox Fruits
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Blox%20Fruits.lua'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Blox%20Fruits.lua'),true))()
 end
 if game.PlaceId == 6284583030 then -- Pet Simulator X
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Pet%20Simulator%20X'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Pet%20Simulator%20X'),true))()
 end
 if game.PlaceId == 9872472334 then -- Evade
- loadstring(game:HttpGet(('https://ppearl.vercel.app'),true))()
+ loadstring(game:HttpGet('https://ppearl.vercel.app'),true))()
 end
 if game.PlaceId == 1962086868 then -- Tower of Hell
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Tower%20of%20Hell'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Tower%20of%20Hell'),true))()
 end
 if game.PlaceId == 3351674303 then -- Driving Empire
- loadstring(game:HttpGet(('https://ppearl.vercel.app'),true))()
+ loadstring(game:HttpGet('https://ppearl.vercel.app'),true))()
 end
 if game.PlaceId == 4566572536 then -- Vehicle Legends
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Vehicle%20Legends'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Vehicle%20Legends'),true))()
 end
 if game.PlaceId == 893973440 then -- Flee the Facility
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Flee%20the%20Facility'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Flee%20the%20Facility'),true))()
 end
 if game.PlaceId == 920587237 then -- Adopt Me
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Adopt%20Me'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Adopt%20Me'),true))()
 end 
 if game.PlaceId == 142823291 then -- Murder Mystery 2
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Murder%20Mystery%202'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Murder%20Mystery%202'),true))()
 end
 if game.PlaceId == 10824616460 then -- Sword Slasher
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Sword%20Slasher.lua'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Sword%20Slasher.lua'),true))()
 end
 if game.PlaceId == 4972091010 then -- Zombie Uprising
- loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Zombie%20Uprising'),true))()
+ loadstring(game:HttpGet('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Zombie%20Uprising'),true))()
 if game.PlaceId == 9551640993 then -- Mining Simulator 2
- loadstring(game:HttpGet(('https://ppearl.vercel.app'),true))()
+ loadstring(game:HttpGet('https://ppearl.vercel.app'),true))()
 end
 if game.PlaceId == 3956818381 then -- Ninja Legends
- loadstring(game:HttpGet(('https://www.obscurity.cf/Loader.lua'),true))()
+ loadstring(game:HttpGet('https://www.obscurity.cf/Loader.lua'),true))()
      end
