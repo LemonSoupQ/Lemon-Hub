03a2dcc12696b867b8422fc7e7693459a2a179aa
LemonSoupQ <126293403+LemonSoupQ@users.noreply.github.com>
2023-02-26 16:08:13 +0200

    Update Lemon Hub.lua

--- Lemon Hub.lua
+++ Lemon Hub.lua
@@ -2,7 +2,7 @@ if game.PlaceId == 286090429 then -- arsenal
  loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Arsenal.lua'),true))()  
 end
 if game.PlaceId == 301549746 then -- CB
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Blox.lua'),true))()
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Counter%20Blox.lua'),true))()
 end
 if game.PlaceId == 1345139196 then -- Treasure hunt simulator
  loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Treasure%20Hunt%20Simulator.lua'),true))()
@@ -14,8 +14,8 @@ if game.PlaceId == 2753915549 then -- Blox Fruits
  loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Blox%20Fruits.lua'),true))()
 end
 if game.PlaceId == 6284583030 then -- PSX
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Pet%20Simulator%20X'),true))()
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Pet%20Simulator%20X'),true))()
 end
 if game.PlaceId == 9872472334 then -- Evade
-
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/LemonSoupQ/Lemon-Hub/Lemon-hub/Evade'),true))()
      end
